7a2ad68bcb0cafde9b617fd2d38c3885fc9bd515
shessafridi <mshesskhan@gmail.com>
2021-11-07 17:41:15 +0500

    Switched back to the custom list tile

--- lib/widgets/recent_chats.dart
+++ lib/widgets/recent_chats.dart
@@ -21,69 +21,7 @@ class RecentChats extends StatelessWidget {
           itemBuilder: (context, index) {
             final chat = chats[index];
 
-            return Container(
-              margin: const EdgeInsets.only(top: 5.0, bottom: 5.0, right: 20.0),
-              padding: const EdgeInsets.symmetric(vertical: 10.0),
-              decoration: BoxDecoration(
-                color: chat.unread ? const Color(0xFFFFEFEE) : Colors.white,
-                borderRadius: const BorderRadius.only(
-                  topRight: Radius.circular(20.0),
-                  bottomRight: Radius.circular(20.0),
-                ),
-              ),
-              child: ListTile(
-                leading: CircleAvatar(
-                  radius: 35,
-                  backgroundImage: AssetImage(chat.sender.imageUrl),
-                ),
-                title: Text(
-                  chat.sender.name,
-                  style: const TextStyle(
-                    color: Colors.grey,
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                subtitle: Text(
-                  chat.text,
-                  style: const TextStyle(
-                    color: Colors.blueGrey,
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.w600,
-                  ),
-                  overflow: TextOverflow.ellipsis,
-                ),
-                trailing: Column(
-                  children: [
-                    Text(
-                      chat.time,
-                      style: const TextStyle(
-                          color: Colors.grey,
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    if (chat.unread)
-                      Container(
-                        margin: const EdgeInsets.only(top: 5),
-                        alignment: Alignment.center,
-                        width: 40,
-                        height: 20,
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).colorScheme.primary,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: const Text(
-                          "New",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 12,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      )
-                  ],
-                ),
-              ),
-            );
+            return buildChatTile(chat, context);
           },
         ),
       ),
@@ -102,46 +40,43 @@ class RecentChats extends StatelessWidget {
         ),
       ),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Row(
-            children: [
-              CircleAvatar(
-                radius: 35,
-                backgroundImage: AssetImage(chat.sender.imageUrl),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    chat.sender.name,
+          CircleAvatar(
+            radius: 35,
+            backgroundImage: AssetImage(chat.sender.imageUrl),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  chat.sender.name,
+                  style: const TextStyle(
+                    color: Colors.grey,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.45,
+                  child: Text(
+                    chat.text,
                     style: const TextStyle(
-                      color: Colors.grey,
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
+                      color: Colors.blueGrey,
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w600,
                     ),
+                    overflow: TextOverflow.ellipsis,
                   ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.45,
-                    child: Text(
-                      chat.text,
-                      style: const TextStyle(
-                        color: Colors.blueGrey,
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.w600,
-                      ),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                ],
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
           Column(
             children: [
